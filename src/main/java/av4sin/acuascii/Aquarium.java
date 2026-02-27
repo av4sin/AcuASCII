a770f2bf4c9671da9380634e819c75bc4d83a778
av4sin <gmondb00@estudiantes.unileon.es>
2026-02-27 01:24:59 +0100

    Modify Aquarium class to use separate width and height attributes; update methods for size management and element addition + toString finished

--- src/main/java/av4sin/acuascii/Aquarium.java
+++ src/main/java/av4sin/acuascii/Aquarium.java
@@ -2,25 +2,51 @@ package av4sin.acuascii;
 
 public class Aquarium {
     
-    private int[] size;
+    private int width;
+    private int height;
     private char[][] aquarium;
 
-    public Aquarium(int weight, int height){
-        this.size[0] = weight;
-        this.size[1] = height;
-        this.aquarium = new char[this.size[0]][this.size[1]];
+    public Aquarium(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.aquarium = new char[this.height][this.width];
+        clear();
     }
 
-    public boolean changeSize(int weight, int height){
-        this.size[0] = weight;
-        this.size[1] = height;
+    public boolean changeSize(int width, int height){
+        this.width = width;
+        this.height = height;
+        this.aquarium = new char[this.height][this.width];
         return true;
     }
 
     public boolean clear(){
-        for(int i=0; i<this.size[0]; i++){
-            for(int j=0; j<this.size[1]; j++){
+        for(int i=0; i<this.height; i++){
+            for(int j=0; j<this.width; j++){
                 this.aquarium[i][j] = ' ';
+            }
+        }
+        return true;
+    }
+
+    public boolean addElement(int x, int y, String asciiString) throws AcuasciiException{
+        if(x>=width || y>=height){
+            throw new AcuasciiException("Los valores de la posición del elemento "+asciiString+" se encuentran fuera de los valores permitidos.");
+        }
+        String[] element = asciiString.split("\n");
+        for(int i=0; i<element.length; i++){
+            int posY = y+i;
+            if(posY >=0 && posY <this.height){
+                for(int j=0; j<element[i].length(); j++){
+                    int posX = x+j;
+                    if(posX>=0 && posX<this.width){
+                        this.aquarium[posY][posX] = element[i].charAt(j);
+                    } else{
+                        return false;
+                    }
+                }
+            } else {
+                return false;
             }
         }
         return true;
@@ -29,6 +55,20 @@ public class Aquarium {
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
+        for(int i=0; i<this.width; i++){
+            sb.append("-");
+        }
+        sb.append("\n");
+        for(int i=0; i<this.height; i++){
+            sb.append("|");
+            for(int j=0; j<this.width; j++){
+                sb.append(aquarium[i][j]);
+            }
+            sb.append("|\n");
+        }
+        for(int i=0; i<this.width; i++){
+            sb.append("-");
+        }
         return sb.toString();
     }
 }
